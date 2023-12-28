887584731877883585fd2b5000b626a1c52e9c0c
lee <lsljw@daum.net>
2023-12-28 21:14:52 +0900

    parallax 2nd

--- js/common.js
+++ js/common.js
@@ -4,18 +4,44 @@ $(document).ready(function(){
     var $opacity=1;
     var $lastScroll;
     var $windowWidth;
+    var $webWidth;
+    var $webPosition;
     var $windowHeight;
+    var $webMoveLeft;
+    var $webTop;
     $windowHeight=$(window).height();
     $("section").css({
         height:$windowHeight
     })
-    $("section:first-child").css({
+    $("#about, #web").css({
         height:2000
     })
+
+    function Position(){
+        $webWidth=$(".web-box").width();
+        $webPosition=($(window).width()-$webWidth)/2
+        // console.log("$webWidth : "+$webWidth)
+        // console.log("webPosition : "+$webPosition)
+        // $(".web-box").css({
+        //     left:$webPosition
+        // })
+        $webMoveLeft=$(window).width();
+        // console.log("$webMoveLeft : "+$webMoveLeft)
+        $webTop=$("section").eq(1).offset().top+$(".web-box").offset().top
+        console.log("$webTop : "+$webTop);
+    }
+    Position();
+
+    $(window).resize(function(){
+        Position();
+    })
+    
+    
     $(window).scroll(function(){
         yScroll=$(window).scrollTop();
+        
         if($lastScroll<yScroll){
-            console.log("스크롤을 내리고 있어요")
+            // console.log("스크롤을 내리고 있어요")
                 
             if($fontSize<=100){
                 if($opacity>=1){
@@ -35,6 +61,20 @@ $(document).ready(function(){
                 console.log("$opacity : "+$opacity)
                 
             }
+            if(yScroll+500>$("section").eq(1).offset().top){
+                // console.log("동작");
+                $webMoveLeft-=25
+                if($webMoveLeft<=$webPosition){
+                    $webMoveLeft=$webPosition; 
+                }
+                $(".web-box").addClass("active").css({
+                    left:$webMoveLeft
+                })
+                console.log("webPosition : "+$webPosition)
+                console.log("webMoveLeft : "+$webMoveLeft)
+            }        
+            
+
         }else{
             console.log("스크롤을 올리고 있어요")
             
@@ -53,12 +93,27 @@ $(document).ready(function(){
                 })
                 console.log("$opacity : "+$opacity)
             }
+            if(yScroll<$webTop+500){
+                console.log("동작");
+                $webMoveLeft+=25
+                if($webMoveLeft>=$(window).width()){
+                    $webMoveLeft=$(window).width();
+                }
+                $(".web-box").css({
+                    left:$webMoveLeft
+                })
+                console.log("webPosition : "+$webPosition)
+                console.log("webMoveLeft : "+$webMoveLeft)
+            }
+
         }
         $lastScroll=yScroll;
-        console.log("스크롤 탑 : "+yScroll);
-        console.log("$fontSize : "+$fontSize)
+        // console.log("스크롤 탑 : "+yScroll);
+        // console.log("$fontSize : "+$fontSize)
         
         
     })
+    
+    
 })//jqeuery 끝
     
